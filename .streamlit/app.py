1e88120fd9aa6405ac94cdcc3d67831cc0f03895
Arokamal <5717+Arokamal@users.github.com>
2023-03-27 21:07:41 +0530

    commit

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -2,7 +2,31 @@ import streamlit as st
 import openai
 import os
 import requests
-from text_summarizer.functions import summarize
+# from text_summarizer.functions import summarize
+
+def summarize(prompt):
+    chunk_size = 8000
+    chunks = [prompt[i:i+chunk_size] for i in range(0, len(prompt), chunk_size)]
+    summary = []
+    for chunk in chunks:
+        augmented_prompt = f"summarize this text: {chunk}"
+    try:
+        response = openai.Completion.create(
+            engine="Daniel",
+            prompt=augmented_prompt,
+            temperature=0.3,
+            max_tokens=250,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            best_of=1,
+            stop=None
+        )
+        summary.append(response.choices[0].text.strip())
+    except Exception as e:
+            st.write('Error:', e)
+            return None
+    return '\n'.join(summary)
 
 try:
   openai.api_type = "azure"
