61a80b4bb2b68dcf301ec4b4b79069bc19cd2ffb
Arokamal <5717+Arokamal@users.github.com>
2023-03-27 21:00:28 +0530

    commit

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -11,27 +11,26 @@ try:
   openai.api_key = os.getenv("OPENAI_API_KEY")
   # openai.api_key = os.getenv('OPENAI_KEY')
   
-  if "summary" not in st.session_state:
-      st.session_state["summary"] = ""
-  
-      st.title("Text Summarizer")
-      input_type = st.radio('Select input type', ('Text', 'URL'))
-      if input_type == 'Text':
-          input_text = st.text_area(label="Enter full text:", value="", height=250)
-          if st.button("Submit"):
-              summary = summarize(input_text)
-              if summary:
-                  st.session_state["summary"] = summary
-                  st.text_area(label="Summarized text:", value=summary, height=250)
-      else:
-          input_url = st.text_input(label="Enter document URL:", value="")
-          if st.button("Submit"):
-              # fetch the document text using requests.get() or any other method
-              document_text = ""
-              summary = summarize(document_text)
-              if summary:
-                  st.session_state["summary"] = summary
-                  st.text_area(label="Summarized text:", value=summary, height=250)
+
+
+  st.title("Text Summarizer")
+  input_type = st.radio('Select input type', ('Text', 'URL'))
+  if input_type == 'Text':
+      input_text = st.text_area(label="Enter full text:", value="", height=250)
+      if st.button("Submit"):
+          summary = summarize(input_text)
+          if summary:
+              st.session_state["summary"] = summary
+              st.text_area(label="Summarized text:", value=summary, height=250)
+  else:
+      input_url = st.text_input(label="Enter document URL:", value="")
+      if st.button("Submit"):
+          # fetch the document text using requests.get() or any other method
+          document_text = ""
+          summary = summarize(document_text)
+          if summary:
+              st.session_state["summary"] = summary
+              st.text_area(label="Summarized text:", value=summary, height=250)
 
 except:
     st.write('There was an error =(')
