428b4aec4c70a1e15fdae1457042e92397597df1
Arokamal <5717+Arokamal@users.github.com>
2023-03-27 19:58:26 +0530

    commit

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 import os
+import requests
 from text_summarizer.functions import summarize
 
 try:
@@ -14,13 +15,33 @@ try:
       st.session_state["summary"] = ""
   
   st.title("Text Summarizer")
-  
-  input_text = st.text_area(label="Enter full text:", value="", height=250)
-  st.button(
-      "Submit",
-      on_click=summarize,
-      kwargs={"prompt": input_text},
+
+  input_type = st.radio(
+      "Select input type:",
+      ("Document link", "Text")
   )
-  output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
+
+  if input_type == "Document link":
+      document_url = st.text_input("Enter document link:")
+      if document_url:
+          response = requests.get(document_url)
+          if response.status_code == 200:
+              input_text = response.text
+              st.write(f"Document summary for: {document_url}")
+              st.write(f"{input_text[:100]}...")
+          else:
+              st.write("Error: Failed to retrieve document from URL.")
+  else:
+      input_text = st.text_area(label="Enter full text:", value="", height=250)
+
+  if input_text:
+      st.button(
+          "Submit",
+          on_click=summarize,
+          kwargs={"prompt": input_text},
+      )
+      output_text = st.text_area(label="Summarized text:", value=st.session_state.get("summary", ""), height=250)
+  else:
+      st.write("Please input text or document link.")
 except:
   st.write('There was an error =(')
