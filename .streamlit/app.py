0e2d7c68bfb387dc091be42da25fb5d16c795491
Arokamal <5717+Arokamal@users.github.com>
2023-03-28 11:55:26 +0530

    commit

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -32,14 +32,12 @@ try:
                 st.write("Error: Failed to retrieve document from URL.")
   if input_type == "Text":
       input_text = st.text_area(label="Enter full text:", value="", height=250)
-
-       
-      input_text:st.button(
-                    "Submit",
-                    on_click=summarize,
-                    kwargs={"prompt": input_text},
-                )
-      output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
+      st.button(
+      "Submit",
+      on_click=summarize,
+      kwargs={"prompt": input_text},
+  )
+  output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
 except:
     st.write("Please input text or document link.")
 
