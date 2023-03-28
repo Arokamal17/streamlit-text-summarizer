077431ad727494499b7b45b291c12cf6fea3a4b2
Arokamal <5717+Arokamal@users.github.com>
2023-03-28 12:43:06 +0530

    commit

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -1,30 +1,23 @@
 import streamlit as st
 import openai
 import os
-import requests
 from text_summarizer.functions import summarize
 
-
-  
 try:
-    openai.api_type = "azure"
-    openai.api_base = "https://chat-gpt-a1.openai.azure.com/"
-    openai.api_version = "2022-12-01"
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    # openai.api_key = os.getenv('OPENAI_KEY')
+  openai.api_key = os.getenv('OPENAI_KEY')
   
-    if "summary" not in st.session_state:
-        st.session_state["summary"] = ""
-    
-    st.title("Text Summarizer")
-    
-    input_text = st.text_area(label="Enter full text:", value="", height=250)
-    st.button(
-        "Submit",
-        on_click=summarize,
-        kwargs={"prompt": input_text},
-    )
-    output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
+  if "summary" not in st.session_state:
+      st.session_state["summary"] = ""
+  
+  st.title("Text Summarizer")
+  
+  input_text = st.text_area(label="Enter full text:", value="", height=250)
+  st.button(
+      "Submit",
+      on_click=summarize,
+      kwargs={"prompt": input_text},
+  )
+  output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
 except:
   st.write('There was an error =(')
 
--- .streamlit/text_summarizer/functions.py
+++ .streamlit/text_summarizer/functions.py
@@ -1,21 +1,14 @@
 import openai
-import os
 import streamlit as st
-
 
 def summarize(prompt):
     augmented_prompt = f"summarize this text: {prompt}"
     try:
         st.session_state["summary"] = openai.Completion.create(
-            engine="text-davinci-003",
+            model="text-davinci-003",
             prompt=augmented_prompt,
-            temperature=0.3,
-            max_tokens=250,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            best_of=1,
-            stop=None
+            temperature=.5,
+            max_tokens=1000,
         )["choices"][0]["text"]
     except:
         st.write('There was an error =(')
