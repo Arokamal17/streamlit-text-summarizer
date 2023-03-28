52c9e5cd7ba858b2dacbc3688aceb5cd7ebc72f1
Arokamal <5717+Arokamal@users.github.com>
2023-03-28 09:38:02 +0530

    commmit

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -9,6 +9,8 @@ try:
   openai.api_base = "https://chat-gpt-a1.openai.azure.com/"
   openai.api_version = "2022-12-01"
   openai.api_key ="c09f91126e51468d88f57cb83a63ee36"
+  if "summary" not in st.session_state:
+      st.session_state["summary"] = ""
   
 
   st.title("Text Summarizer")
@@ -20,7 +22,7 @@ try:
 
   if input_type == "Document link":
         document_url = st.text_input("Enter document link:")
-        if document_url:
+        if st.button("Submit"):
             response = requests.get(document_url)
             if response.status_code == 200:
                 input_text = response.text
@@ -28,16 +30,16 @@ try:
                 st.write(f"{input_text[:100]}...")
             else:
                 st.write("Error: Failed to retrieve document from URL.")
-  else:
-        input_text = st.text_area(label="Enter full text:", value="", height=250)
+  if input_type == "Text":
+      input_text = st.text_area(label="Enter full text:", value="", height=250)
 
        
-  if input_text:st.button(
-            "Submit",
-            on_click=summarize,
-            kwargs={"prompt": input_text},
-        )
-  output_text = st.text_area(label="Summarized text:", value=st.session_state.get("summary", ""), height=250)
+      input_text:st.button(
+                    "Submit",
+                    on_click=summarize,
+                    kwargs={"prompt": input_text},
+                )
+      output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
 except:
     st.write("Please input text or document link.")
 
